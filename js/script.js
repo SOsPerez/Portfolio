4346f00b57276f2362a0fd3a58897de225dac58b
soswaldopr <Sergio.Perez@theksquaregroup.com>
2021-05-15 17:05:23 -0500

    Dark mode added

--- js/script.js
+++ js/script.js
@@ -1,23 +1,24 @@
+function CopyToClipboard(id){
+    let r = document.createRange();
+    r.selectNode(document.getElementById(id));
+    window.getSelection().removeAllRanges();
+    window.getSelection().addRange(r);
+    document.execCommand('copy');
+    window.getSelection().removeAllRanges();
+}
 
+$(function () {
+    $("#myEmail").tooltip();
+})
+let navMode="navbar-light";
 
-    function darkMode() {
-        let element = document.body;
-        element.classList.toggle("dark-mode");
+let mode = document.getElementById("mode");
+mode.onclick = function(){
+    document.body.classList.toggle("darkMode");
+    if(document.body.classList.contains("darkMode")){
+        mode.src ="images/sun.png";
+    }else{
+        mode.src ="images/moon.png";
     }
-    
-    
-    function CopyToClipboard(id){
-        let r = document.createRange();
-        r.selectNode(document.getElementById(id));
-        window.getSelection().removeAllRanges();
-        window.getSelection().addRange(r);
-        document.execCommand('copy');
-        window.getSelection().removeAllRanges();
-    }
-    
-    $(function () {
-        $("#myEmail").tooltip();
-    })
-
-
+}
 
